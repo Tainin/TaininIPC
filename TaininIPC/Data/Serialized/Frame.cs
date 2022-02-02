f47bdcce169eb4698b6cae7b46a530b604e9d4a2
Jakob Schechter <jakeeli52@gmail.com>
2022-02-02 17:12:16 -0500

    Added Frame.cs Frame represents serialized data to be sent over the network.

---
+++ TaininIPC/Data/Serialized/Frame.cs
@@ -0,0 +1,61 @@
+﻿namespace TaininIPC.Data.Serialized;
+
+public sealed class Frame {
+    private sealed class Node { 
+        public Node? Previous { get; set; }
+        public Node? Next { get; set; }
+        public ReadOnlyMemory<byte> Data { get; set; }
+        public Node(ReadOnlyMemory<byte> data) => Data = data;
+    }
+
+    private readonly Node preStart;
+    private readonly Node postEnd;
+
+    public Frame() {
+        preStart = new(ReadOnlyMemory<byte>.Empty);
+        postEnd = new(ReadOnlyMemory<byte>.Empty);
+        (preStart.Next, postEnd.Previous) = (postEnd, preStart);
+    }
+    public void Insert(ReadOnlyMemory<byte> data, Index index) {
+        Node newNode = new(data);
+
+        (Node prev, Func<Node, Node?> traverse) = index.IsFromEnd ?
+            (postEnd, (Func<Node, Node?>)(node => node.Previous)) :
+            (preStart, node => node.Next);
+
+        int limit = index.Value;
+        for (int i = 0; i < limit; i++)
+            prev = traverse(prev) ?? throw new IndexOutOfRangeException();
+
+        Node next = prev.Next ?? throw new IndexOutOfRangeException();
+
+        (newNode.Next, newNode.Previous) = (next, prev);
+        next.Previous = newNode;
+        prev.Next = newNode;
+    }
+    public ReadOnlyMemory<byte> Get(Index index) => Find(index, remove: false).Data;
+    public ReadOnlyMemory<byte> Pop(Index index) => Find(index, remove: true).Data;
+    public void Remove(Index index) => Find(index, remove: true);
+    public void Clear() => (preStart.Next, postEnd.Previous) = (postEnd, preStart);
+    public bool IsEmpty() => ReferenceEquals(preStart.Next, postEnd) || ReferenceEquals(postEnd.Previous, preStart);
+
+    private Node Find(Index index, bool remove) {
+        (Node curr, Func<Node, Node?> traverse) = index.IsFromEnd ?
+            (postEnd, (Func<Node, Node?>)(node => node.Previous)) :
+            (preStart.Next!, node => node.Next);
+
+        int limit = index.Value;
+        for (int i = 0; i < limit; i++)
+            curr = traverse(curr) ?? throw new IndexOutOfRangeException();
+
+        if (curr.Next is null || curr.Previous is null)
+            throw new IndexOutOfRangeException();
+
+        if (!remove) return curr;
+
+        (curr.Next.Previous, curr.Previous.Next) = (curr.Previous, curr.Next);
+        (curr.Next, curr.Previous) = (null, null);
+
+        return curr;
+    }
+}

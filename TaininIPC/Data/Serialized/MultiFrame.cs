3c07b1c941dcebbc8f5f821918c749a878c55a62
Jakob Schechter <jakeeli52@gmail.com>
2022-02-03 21:09:15 -0500

    Updated key/id conversion in MultiFrame.cs

--- TaininIPC/Data/Serialized/MultiFrame.cs
+++ TaininIPC/Data/Serialized/MultiFrame.cs
@@ -7,31 +7,33 @@ public sealed class MultiFrame {
 
     private readonly CritBitTree<Frame> subFrames;
 
-    public MultiFrame() => subFrames = new(); 
+    public MultiFrame() => subFrames = new();
 
     public IEnumerable<Frame> SubFrames => subFrames.Values;
     public IEnumerable<short> Keys => 
-        subFrames.Keys.Select(rom => BinaryPrimitives.ReadInt16BigEndian(rom.Span));
+        subFrames.Keys.Select(rom => GetId(rom.Span));
     public IEnumerable<(Frame Frame, short Key)> Pairs =>
-        subFrames.Pairs.Select(pair => (pair.Value, BinaryPrimitives.ReadInt16BigEndian(pair.Key.Span)));
+        subFrames.Pairs.Select(pair => (pair.Value, GetId(pair.Key.Span)));
 
     public Frame Create(short id) {
         Frame frame = new();
-        if (subFrames.TryAdd(GetKeyBuffer(id), frame)) return frame;
+        if (subFrames.TryAdd(GetKey(id), frame)) return frame;
         else throw new InvalidOperationException();
     }
-    public Frame Get(short id) => subFrames.TryGet(GetKeyBuffer(id), out Frame? frame) ? frame! : 
+    public Frame Get(short id) => subFrames.TryGet(GetKey(id).Span, out Frame? frame) ? frame! : 
         throw new InvalidOperationException();
-    public bool ContainsKey(short id) => subFrames.ContainsKey(GetKeyBuffer(id));
+    public bool ContainsKey(short id) => subFrames.ContainsKey(GetKey(id).Span);
     public void Remove(short id) {
-        if (subFrames.TryRemove(GetKeyBuffer(id))) return;
+        if (subFrames.TryRemove(GetKey(id).Span)) return;
         else throw new InvalidOperationException();
     }
     public void Clear() => subFrames.Clear();
 
-    private static byte[] GetKeyBuffer(short id) {
+    private static ReadOnlyMemory<byte> GetKey(short id) {
         byte[] key = new byte[sizeof(short)];
         BinaryPrimitives.WriteInt16BigEndian(key, id);
         return key;
     }
+    private static short GetId(ReadOnlySpan<byte> key) =>
+        BinaryPrimitives.ReadInt16BigEndian(key);
 }

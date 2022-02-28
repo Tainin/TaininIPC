69930245cbf64ba6a6fa53d41ccf5edc5bbd9254
Jakob Schechter <jakeeli52@gmail.com>
2022-02-28 12:42:22 -0500

    Updated RoutingTable - Added delegate field to allow caller to provide the method of extracting the routing key.

--- TaininIPC/Client/RoutingTable.cs
+++ TaininIPC/Client/RoutingTable.cs
@@ -5,10 +5,15 @@ using TaininIPC.Data.Serialized;
 namespace TaininIPC.Client;
 
 public sealed class RoutingTable : AbstractTable<IRouter, IRouter>,  IRouter {
-    public RoutingTable(int reservedCount) : base(reservedCount) { }
+
+    public delegate ReadOnlyMemory<byte> KeyExtractor(MultiFrame frame);
+
+    private readonly KeyExtractor keyExtractor;
+
+    public RoutingTable(int reservedCount, KeyExtractor keyExtractor) : base(reservedCount) => this.keyExtractor = keyExtractor;
     protected override Task<int> AddInternal(IRouter input, int id) => AddInternalBase(input, id);
     public async Task RouteFrame(MultiFrame frame, EndpointTableEntry origin) {
-        (IRouter? router, bool got) = await GetInternal(frame.Get(-1).Rotate()).ConfigureAwait(false);
+        (IRouter? router, bool got) = await GetInternal(keyExtractor(frame)).ConfigureAwait(false);
         if (got && router is not null) await router.RouteFrame(frame, origin).ConfigureAwait(false);
     }
 }

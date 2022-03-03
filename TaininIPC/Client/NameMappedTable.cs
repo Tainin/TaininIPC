ca801bd92a5ee662d59fdb9aac1870cb49a43aa0
Jakob Schechter <jakeeli52@gmail.com>
2022-03-02 19:40:32 -0500

    Added support for unnamed entries in NameMappedTable.

--- TaininIPC/Client/NameMappedTable.cs
+++ TaininIPC/Client/NameMappedTable.cs
@@ -16,22 +16,27 @@ public class NameMappedTable<TableType, TInput, TStored> where TableType : ITabl
         internalTable = table;
     }
 
-    public Task<int> Add(string name, TInput input) => AddInternal(name, 0, input, reserved: false);
-    public Task AddReserved(string name, int id, TInput input) => AddInternal(name, id, input, reserved: true);
+    public Task<int> Add(string name, TInput input) => AddInternal(name, 0, input, reserved: false, named: true);
+    public Task<int> AddUnnamed(TInput input) => AddInternal(string.Empty, 0, input, reserved: false, named: false);
+    public Task AddReserved(string name, int id, TInput input) => AddInternal(name, id, input, reserved: true, named: true);
+    public Task AddReservedUnnamed(int id, TInput input) => AddInternal(string.Empty, id, input, reserved: true, named: false);
+
     public async Task Clear() {
         await syncSemaphore.WaitAsync().ConfigureAwait(false);
         await internalTable.Clear().ConfigureAwait(false);
         nameMap.Clear();
         syncSemaphore.Release();
     }
+
     public Task<TStored> Get(int id) => GetInternal(string.Empty, id, ReadOnlyMemory<byte>.Empty);
     public Task<TStored> Get(ReadOnlyMemory<byte> key) => GetInternal(string.Empty, 0, key);
     public Task<TStored> Get(string name) => GetInternal(name, 0, ReadOnlyMemory<byte>.Empty);
+
     public Task Remove(int id) => RemoveInternal(string.Empty, id, ReadOnlyMemory<byte>.Empty);
     public Task Remove(ReadOnlyMemory<byte> key) => RemoveInternal(string.Empty, 0, key);
     public Task Remove(string name) => RemoveInternal(name, 0, ReadOnlyMemory<byte>.Empty);
 
-    private async Task<int> AddInternal(string name, int id, TInput input, bool reserved) {
+    private async Task<int> AddInternal(string name, int id, TInput input, bool reserved, bool named) {
         if (string.IsNullOrEmpty(name)) 
             throw new ArgumentException("Name must not be null or empty", nameof(name));
         ReadOnlyMemory<byte> nameKey = Encoding.UTF8.GetBytes(name);
@@ -40,7 +45,11 @@ public class NameMappedTable<TableType, TInput, TStored> where TableType : ITabl
             if (reserved) await internalTable.AddReserved(input, id).ConfigureAwait(false);
             else id = await internalTable.Add(input).ConfigureAwait(false);
 
+            if (!named) return id;
+
             if (nameMap.TryAdd(nameKey, id)) return id;
+
+            await internalTable.Remove(id).ConfigureAwait(false);
             throw new ArgumentException($"The {nameof(NameMappedTable<TableType, TInput, TStored>)} " +
                 $"alreay contains an entry with the provided name.", nameof(name));
         } finally {
@@ -53,7 +62,14 @@ public class NameMappedTable<TableType, TInput, TStored> where TableType : ITabl
             if (!key.IsEmpty) return await internalTable.Get(key).ConfigureAwait(false);
 
             if (!string.IsNullOrEmpty(name)) id = GetIdFromString(name);
-            return await internalTable.Get(id).ConfigureAwait(false);
+
+            try {
+                return await internalTable.Get(id).ConfigureAwait(false);
+            } catch (ArgumentException) {
+                nameMap.TryRemove(Encoding.UTF8.GetBytes(name));
+                throw;
+            }
+            
         } finally {
             syncSemaphore.Release();
         }
@@ -64,8 +80,9 @@ public class NameMappedTable<TableType, TInput, TStored> where TableType : ITabl
             if (key.IsEmpty) {
                 if (!string.IsNullOrEmpty(name)) id = GetIdFromString(name);
                 await internalTable.Remove(id).ConfigureAwait(false);
-            } else  await internalTable.Remove(key).ConfigureAwait(false);
+            } else await internalTable.Remove(key).ConfigureAwait(false);
         } finally {
+            nameMap.TryRemove(Encoding.UTF8.GetBytes(name));
             syncSemaphore.Release();
         }
     }

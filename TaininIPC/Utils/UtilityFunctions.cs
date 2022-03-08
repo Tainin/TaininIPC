8e85c7af7ff55a2255e7b8ad247a2ebefa6169e9
Jakob Schechter <jakeeli52@gmail.com>
2022-03-08 14:34:15 -0500

    Added comments to UtilityFunctions.cs

--- TaininIPC/Utils/UtilityFunctions.cs
+++ TaininIPC/Utils/UtilityFunctions.cs
@@ -1,6 +1,15 @@
 ﻿namespace TaininIPC.Utils;
 
+/// <summary>
+/// Static class containing general utility functions.
+/// </summary>
 public static class UtilityFunctions {
+    /// <summary>
+    /// Utility function which sets <paramref name="defaultValue"/> to the default value of <typeparamref name="T"/> and returns false.
+    /// </summary>
+    /// <typeparam name="T">The type of <paramref name="defaultValue"/>.</typeparam>
+    /// <param name="defaultValue">Out parameter which will be set to the default value of <typeparamref name="T"/>.</param>
+    /// <returns><c>false</c></returns>
     public static bool DefaultAndFalse<T>(out T? defaultValue) {
         defaultValue = default;
         return false;

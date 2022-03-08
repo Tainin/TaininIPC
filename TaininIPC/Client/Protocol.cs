e22a801f1d8480001b589bb0fb33c2652e27ffcf
Jakob Schechter <jakeeli52@gmail.com>
2022-03-07 22:28:50 -0500

    Added CallResponseHandler

--- TaininIPC/Client/Protocol.cs
+++ TaininIPC/Client/Protocol.cs
@@ -4,4 +4,11 @@ namespace TaininIPC.Client;
 
 public static class Protocol {
     public static ReadOnlyMemory<byte> ExtractRoutingKey(MultiFrame frame) => frame.Get(-1).Rotate();
+
+    public static ReadOnlyMemory<byte> GetResponseKey(MultiFrame frame) => frame.Get(-2).Get(0);
+    public static void SetResponseKey(MultiFrame frame, ReadOnlyMemory<byte> key) {
+        Frame responseInfoFrame = frame.Get(-2);
+        if (responseInfoFrame.Length < 1) responseInfoFrame.Insert(key, 0);
+        else responseInfoFrame.Swap(0, key);
+    }
 }
---
+++ TaininIPC/Client/RPC/CallResponseHandler.cs
@@ -0,0 +1,90 @@
+﻿using System.Buffers.Binary;
+using System.Diagnostics;
+using TaininIPC.Client.Endpoints;
+using TaininIPC.Client.Interface;
+using TaininIPC.Data.Serialized;
+using TaininIPC.Utils;
+
+namespace TaininIPC.Client.RPC;
+
+public sealed class CallResponseHandler : IRouter {
+    private sealed class ResponseHandle {
+        private readonly SemaphoreSlim whenSemaphore;
+        private MultiFrame response = null!;
+
+        public ResponseHandle() => whenSemaphore = new(0, 1);
+
+        public void Release(MultiFrame frame) {
+            response = frame;
+            whenSemaphore.Release();
+        }
+
+        public async Task<MultiFrame> WhenResponse() {
+            await whenSemaphore.WaitAsync().ConfigureAwait(false);
+            return response;
+        }
+    }
+
+    private readonly CritBitTree<ResponseHandle> responseHandlers;
+    private readonly Queue<int> indexRecycling;
+    private readonly Queue<ResponseHandle> handleRecycling;
+    private readonly SemaphoreSlim syncSemaphore;
+
+    private int nextFreshIndex;
+
+    public CallResponseHandler() {
+        (responseHandlers, indexRecycling, handleRecycling) = (new(), new(), new());
+        syncSemaphore = new(1, 1);
+        nextFreshIndex = 0;
+    }
+
+    public Task RouteFrame(MultiFrame frame, EndpointTableEntry _) {
+        if (responseHandlers.TryGet(Protocol.GetResponseKey(frame).Span, out ResponseHandle? handle))
+            if (handle is not null) handle.Release(frame);
+        return Task.CompletedTask;
+    }
+    public async Task<MultiFrame> Call(EndpointTableEntry endpointTableEntry, MultiFrame frame) {
+        (ResponseHandle handle, int index, ReadOnlyMemory<byte> responseKey) = 
+            await SetupResponseHandler().ConfigureAwait(false);
+        
+        Protocol.SetResponseKey(frame, responseKey);
+        await endpointTableEntry.FrameEndpoint.SendMultiFrame(frame).ConfigureAwait(false);
+        MultiFrame response = await handle.WhenResponse().ConfigureAwait(false);
+
+        await TearDownResponseHandler(handle, index, responseKey).ConfigureAwait(false);
+
+        return response;
+    }
+
+    private async Task<(ResponseHandle handle, int index, ReadOnlyMemory<byte> responseKey)> SetupResponseHandler() {
+        await syncSemaphore.WaitAsync().ConfigureAwait(false);
+        if (!handleRecycling.TryDequeue(out ResponseHandle? handle) || handle is null) handle = new();
+        if (!indexRecycling.TryDequeue(out int index)) index = Interlocked.Increment(ref nextFreshIndex);
+
+        byte[] responseKey = new byte[2 * sizeof(int)];
+        BinaryPrimitives.WriteInt32BigEndian(responseKey.AsSpan(0 * sizeof(int)), index);
+        BinaryPrimitives.WriteInt32BigEndian(responseKey.AsSpan(1 * sizeof(int)), Environment.TickCount);
+
+        bool added = responseHandlers.TryAdd(responseKey, handle);
+
+        syncSemaphore.Release();
+
+        Debug.Assert(added, $"{nameof(CallResponseHandler)}.{nameof(Call)} should never fail " +
+            $"to add {nameof(handle)} to {nameof(responseHandlers)}");
+        
+        return (handle, index, responseKey);
+    }
+    private async Task TearDownResponseHandler(ResponseHandle handle, int index, ReadOnlyMemory<byte> responseKey) {
+        await syncSemaphore.WaitAsync().ConfigureAwait(false);
+
+        bool removed = responseHandlers.TryRemove(responseKey.Span);
+
+        handleRecycling.Enqueue(handle);
+        indexRecycling.Enqueue(index);
+
+        syncSemaphore.Release();
+
+        Debug.Assert(removed, $"{nameof(CallResponseHandler)}.{nameof(Call)} should never fail " +
+            $"to remove {nameof(handle)} from {nameof(responseHandlers)}");
+    }
+}

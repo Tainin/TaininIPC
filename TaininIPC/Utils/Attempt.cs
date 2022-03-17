9261ca1778b473030169274efbdfc3ea91fc3399
Jakob Schechter <jakeeli52@gmail.com>
2022-03-16 18:30:18 -0600

    Added Attempt class

---
+++ TaininIPC/Utils/Attempt.cs
@@ -0,0 +1,53 @@
+﻿namespace TaininIPC.Utils;
+
+/// <summary>
+/// Represents a potential result in <see cref="Task"/> based <c>Try(Something)</c> style methods where <see langword="out"/>
+/// parameters are not permitted.
+/// </summary>
+/// <typeparam name="T">The type of the result.</typeparam>
+public sealed class Attempt<T> {
+
+    /// <summary>
+    /// Represents a failed <see cref="Attempt{T}"/>
+    /// </summary>
+    public static Attempt<T> Failed { get; } = new Attempt<T>();
+
+    /// <summary>
+    /// The result of the operation if it was successful.
+    /// </summary>
+    public T? Result { get; }
+    /// <summary>
+    /// Indicates whether or not there is a result.
+    /// </summary>
+    public bool HasResult { get; } = false;
+
+    /// <summary>
+    /// Initializes a new <see cref="Attempt{T}"/> given the <paramref name="result"/> of the attempt.
+    /// </summary>
+    /// <param name="result">The result of the attempt.</param>
+    public Attempt(T result) => (Result, HasResult) = (result, true);
+    /// <summary>
+    /// Initializes a new <see cref="Attempt{T}"/> without a result.
+    /// </summary>
+    public Attempt() { }
+
+    /// <summary>
+    /// Attempts to get the result of the <see cref="Attempt{T}"/>.
+    /// </summary>
+    /// <param name="result">Contains the <see cref="Result"/> on return if the <see cref="Attempt{T}"/> was initialized with one.
+    /// Otherwise is undefined.</param>
+    /// <returns><see langword="true"/> if the <see cref="Attempt{T}"/> was initialized with a result, 
+    /// <see langword="false"/> otherwise.</returns>
+    public bool TryResult(out T? result) {
+        if (!HasResult) return UtilityFunctions.DefaultAndFalse(out result);
+        result = Result;
+        return true;
+    }
+
+    /// <summary>
+    /// Defines an implicit conversion of a <typeparamref name="T"/> to an <see cref="Attempt{T}"/> 
+    /// where <see cref="T"/> is <typeparamref name="T"/>
+    /// </summary>
+    /// <param name="result"></param>
+    public static implicit operator Attempt<T>(T result) => new(result);
+}

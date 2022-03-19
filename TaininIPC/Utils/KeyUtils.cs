a5c2ad6fed9cb38fde9794b8fc3dd520fbae5eab
Jakob Schechter <jakeeli52@gmail.com>
2022-03-19 08:57:58 -0600

    Added functions for converting string names to and from keys to KeyUtils.

--- TaininIPC/Utils/KeyUtils.cs
+++ TaininIPC/Utils/KeyUtils.cs
@@ -1,4 +1,5 @@
 ﻿using System.Buffers.Binary;
+using System.Text;
 
 namespace TaininIPC.Utils;
 
@@ -30,15 +31,30 @@ public static class KeyUtils {
     }
 
     /// <summary>
-    /// Utility function which converts a key back into it's <see langword="int"/> id form.
+    /// Utility function which converts the given <paramref name="name"/> into a key.
+    /// </summary>
+    /// <param name="name">The name to convert.</param>
+    /// <returns>The key representation of the <paramref name="name"/>.</returns>
+    public static ReadOnlyMemory<byte> GetKey(string name) => Encoding.BigEndianUnicode.GetBytes(name);
+
+    /// <summary>
+    /// Utility function which converts the given <paramref name="key"/> back into it's <see langword="int"/> id form.
     /// </summary>
     /// <param name="key">The key to convert.</param>
     /// <returns>The id form of the given <paramref name="key"/>.</returns>
     public static int GetIntId(ReadOnlyMemory<byte> key) => BinaryPrimitives.ReadInt32BigEndian(key.Span);
+
     /// <summary>
-    /// Utility function which converts a key back into it's <see langword="short"/> id form.
+    /// Utility function which converts the given <paramref name="key"/> back into it's <see langword="short"/> id form.
     /// </summary>
     /// <param name="key">The key to convert.</param>
     /// <returns>The id form of the given <paramref name="key"/>.</returns>
     public static short GetShortId(ReadOnlyMemory<byte> key) => BinaryPrimitives.ReadInt16BigEndian(key.Span);
+
+    /// <summary>
+    /// Utility function which converts the given <paramref name="key"/> back into it's <see langword="string"/> name form.
+    /// </summary>
+    /// <param name="key">The key to convert.</param>
+    /// <returns>The name form of the given <paramref name="key"/></returns>
+    public static string GetName(ReadOnlyMemory<byte> key) => Encoding.BigEndianUnicode.GetString(key.Span);
 }

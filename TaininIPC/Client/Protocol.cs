a8129386734d4d1188b4f0fa9506774ef938407c
Jakob Schechter <jakeeli52@gmail.com>
2022-03-23 14:28:54 -0400

    Updated Protocol.cs functions to use new MultiFrame interface.

--- TaininIPC/Client/Protocol.cs
+++ TaininIPC/Client/Protocol.cs
@@ -1,14 +1,31 @@
-﻿using TaininIPC.Data.Serialized;
+﻿using System.Diagnostics.CodeAnalysis;
+using TaininIPC.CritBitTree.Keys;
+using TaininIPC.Data.Serialized;
+using TaininIPC.Utils;
 
 namespace TaininIPC.Client;
 
 public static class Protocol {
-    public static ReadOnlyMemory<byte> ExtractRoutingKey(MultiFrame frame) => frame.Get(-1).Rotate();
 
-    public static ReadOnlyMemory<byte> GetResponseKey(MultiFrame frame) => frame.Get(-2).Get(0);
-    public static void SetResponseKey(MultiFrame frame, ReadOnlyMemory<byte> key) {
-        Frame responseInfoFrame = frame.Get(-2);
-        if (responseInfoFrame.Length < 1) responseInfoFrame.Insert(key, 0);
-        else responseInfoFrame.Swap(0, key);
+    private static readonly Int16Key KEY_ROUTING_KEY = new(-1);
+    private static readonly Int16Key KEY_RESPONSE_KEY = new(-2);
+
+    public static bool TryGetRoutingKey(MultiFrame frame, [NotNullWhen(true)]out Int32Key? routingKey) {
+        if (!frame.TryGet(KEY_ROUTING_KEY, out Frame? subFrame)) return UtilityFunctions.DefaultAndFalse(out routingKey);
+        routingKey = new(subFrame.Rotate());
+        return true;
+    }
+
+    public static bool TryGetResponseKey(MultiFrame frame, [NotNullWhen(true)]out BasicKey? responseKey) {
+        if (!frame.TryGet(KEY_RESPONSE_KEY, out Frame? subFrame)) return UtilityFunctions.DefaultAndFalse(out responseKey);
+        responseKey = new(subFrame.Get(0));
+        return true;
+    }
+    public static bool SetResponseKey(MultiFrame frame, BasicKey key) {
+        if (!frame.TryGet(KEY_RESPONSE_KEY, out Frame? subFrame)) return false;
+
+        if (subFrame.Length < 1) subFrame.Insert(key.Memory, 0);
+        else subFrame.Swap(0, key.Memory);
+        return true;
     }
 }

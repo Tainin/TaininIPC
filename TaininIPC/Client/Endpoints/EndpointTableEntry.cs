544931cd92a632296d1118af34740d757d3fa4b7
Jakob Schechter <jakeeli52@gmail.com>
2022-03-24 21:08:39 -0400

    Fixed change which was not supposed to be in last commit.

--- TaininIPC/Client/Endpoints/EndpointTableEntry.cs
+++ TaininIPC/Client/Endpoints/EndpointTableEntry.cs
@@ -11,6 +11,15 @@ namespace TaininIPC.Client.Endpoints;
 /// Represents an entry in an <see cref="Endpoints.EndpointTable"/> or <see cref="NameMappedEndpointTable"/>.
 /// </summary>
 public sealed class EndpointTableEntry {
+    
+    /// <summary>
+    /// The <see cref="INetworkEndpoint"/> which handles sending and receiving <see cref="NetworkChunk"/> instances for the entry.
+    /// </summary>
+    public INetworkEndpoint NetworkEndpoint { get; private init; }
+    /// <summary>
+    /// The <see cref="Network.FrameEndpoint"/> which handles sending and receiveing <see cref="MultiFrame"/> instances for the entry.
+    /// </summary>
+    public FrameEndpoint FrameEndpoint { get; private init; }
     /// <summary>
     /// The entry's containing table.
     /// </summary>
@@ -24,9 +33,6 @@ public sealed class EndpointTableEntry {
     /// </summary>
     public Int32Key Key { get; private init; }
 
-    private readonly INetworkEndpoint networkEndpoint;
-    private readonly FrameEndpoint frameEndpoint;
-
     /// <summary>
     /// Initializes an <see cref="EndpointTableEntry"/> from it's containing <paramref name="endpointTable"/>, it's
     /// <paramref name="key"/> in that table, and an <paramref name="options"/> object.
@@ -35,15 +41,15 @@ public sealed class EndpointTableEntry {
     /// <param name="endpointTable">The <see cref="Endpoints.EndpointTable"/> to which the entry belongs.</param>
     /// <param name="options">The options to use when initializing the entry.</param>
     public EndpointTableEntry(Int32Key key, EndpointTable endpointTable, EndpointTableEntryOptions options) {
-        networkEndpoint = options.NetworkFactory(HandleIncomingChunk);
-        frameEndpoint = new(HandleOutgoingChunk, HandleIncomingFrame);
+        NetworkEndpoint = options.NetworkFactory(HandleIncomingChunk);
+        FrameEndpoint = new(HandleOutgoingChunk, HandleIncomingFrame);
 
         EndpointTable = endpointTable;
         Router = options.Router;
         Key = key;
     }
 
-    private Task HandleOutgoingChunk(NetworkChunk chunk) => networkEndpoint.SendChunk(chunk);
-    private Task HandleIncomingChunk(NetworkChunk chunk) => frameEndpoint.ApplyChunk(chunk);
+    private Task HandleOutgoingChunk(NetworkChunk chunk) => NetworkEndpoint.SendChunk(chunk);
+    private Task HandleIncomingChunk(NetworkChunk chunk) => FrameEndpoint.ApplyChunk(chunk);
     private Task HandleIncomingFrame(MultiFrame multiFrame) => Router.RouteFrame(multiFrame, this);
 }

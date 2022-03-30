86f4f8280660c060f36e530de355212382da6879
Jakob Schechter <jakeeli52@gmail.com>
2022-03-30 18:03:47 -0400

    Renamed some static fields of ProtocolHelper

--- TaininIPC/Protocol/ProtocolHelper.cs
+++ TaininIPC/Protocol/ProtocolHelper.cs
@@ -10,8 +10,9 @@ namespace TaininIPC.Protocol;
 /// </summary>
 public static class ProtocolHelper {
 
-    private static readonly Int16Key KEY_ROUTING_KEY = new(-1);
-    private static readonly Int16Key KEY_RESPONSE_KEY = new(-2);
+    private static readonly Int16Key ROUTING_PATH_KEY = new(-1);
+    // TODO: RETURN_PATH_KEY
+    private static readonly Int16Key RESPONSE_IDENTIFIER_KEY = new(-3);
 
     /// <summary>
     /// Retrieves, from the given <paramref name="frame"/>, the next routing key to use to route it.
@@ -20,7 +21,7 @@ public static class ProtocolHelper {
     /// <param name="routingKey">The retrieved routing key if found.</param>
     /// <returns><see langword="true"/> if the <paramref name="routingKey"/> was found, <see langword="false"/> otherwise.</returns>
     public static bool TryGetRoutingKey(MultiFrame frame, [NotNullWhen(true)]out Int32Key? routingKey) {
-        if (!frame.TryGet(KEY_ROUTING_KEY, out Frame? subFrame)) return UtilityFunctions.DefaultAndFalse(out routingKey);
+        if (!frame.TryGet(ROUTING_PATH_KEY, out Frame? subFrame)) return UtilityFunctions.DefaultAndFalse(out routingKey);
         routingKey = new(subFrame.Rotate());
         return true;
     }
@@ -32,7 +33,7 @@ public static class ProtocolHelper {
     /// <param name="responseKey">The retrieved response key if found.</param>
     /// <returns><see langword="true"/> if the <paramref name="responseKey"/> was found, <see langword="false"/> otherwise.</returns>
     public static bool TryGetResponseKey(MultiFrame frame, [NotNullWhen(true)]out BasicKey? responseKey) {
-        if (!frame.TryGet(KEY_RESPONSE_KEY, out Frame? subFrame)) return UtilityFunctions.DefaultAndFalse(out responseKey);
+        if (!frame.TryGet(RESPONSE_IDENTIFIER_KEY, out Frame? subFrame)) return UtilityFunctions.DefaultAndFalse(out responseKey);
         responseKey = new(subFrame.Get(0));
         return true;
     }
@@ -44,8 +45,8 @@ public static class ProtocolHelper {
     /// <param name="frame">The frame to write the <paramref name="key"/> into.</param>
     /// <param name="key">The key to write into the <paramref name="frame"/>.</param>
     public static void SetResponseKey(MultiFrame frame, BasicKey key) {
-        if (!frame.TryGet(KEY_RESPONSE_KEY, out Frame? subFrame))
-            frame.TryCreate(KEY_RESPONSE_KEY, out subFrame);
+        if (!frame.TryGet(RESPONSE_IDENTIFIER_KEY, out Frame? subFrame))
+            frame.TryCreate(RESPONSE_IDENTIFIER_KEY, out subFrame);
 
         if (subFrame.Length < 1) subFrame.Insert(key.Memory, 0);
         else subFrame.Swap(0, key.Memory);
